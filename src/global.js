0d3fc25dd1a680d2662961fea9cda0627b2c5c5c
Moritz Gunz <moritz.gunz@gmail.com>
2018-05-07 15:14:46 +0200

    Fix syntax errors in SystemJS builds (#12)

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-export default typeof global !== "undefined" ? global :
+export default (typeof global !== "undefined" ? global :
             typeof self !== "undefined" ? self :
-            typeof window !== "undefined" ? window : {}
+            typeof window !== "undefined" ? window : {});
